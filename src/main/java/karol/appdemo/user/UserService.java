5df4e5bbaa2c907cdab280838b561ce4f801cb19
CrossKarol <karolekkawecki@o2.pl>
2019-03-07 15:46:09 +0100

    Pobranie listy użytkowników wraz z danymi w panelu administratora.

--- src/main/java/karol/appdemo/user/UserService.java
+++ src/main/java/karol/appdemo/user/UserService.java
@@ -1,9 +1,12 @@
 package karol.appdemo.user;
 
+import java.util.List;
+
 public interface UserService {
 
     public User findUserByEmail(String email);
     public void saveUser(User user);
     public void updateUserPassword(String newPassword, String email);
     public void updateUserProfile(String newName, String newLastName, String newEmail, int id);
+    public List<User> findAll();
 }
